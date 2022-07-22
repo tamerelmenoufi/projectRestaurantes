8a281ed06378d6b232abf07367e98ad6fe4721bc
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-21 23:55:34 -0400

    instalação

--- src/componentes/menu_lateral/menu.php
+++ src/componentes/menu_lateral/menu.php
@@ -1,5 +1,5 @@
 <?php
-    include("{$_SERVER['DOCUMENT_ROOT']}/app/pesquisaRestaurantes/lib/includes.php");
+    include("{$_SERVER['DOCUMENT_ROOT']}/app/projectRestaurantes/lib/includes.php");
 ?>
 <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
   <div class="offcanvas-header">
@@ -17,52 +17,7 @@
       </div>
     </div>
 
-    <!-- <div class="row mb-1">
-      <div class="col">
-        <a url="src/os/index.php" class="text-decoration-none" data-bs-dismiss="offcanvas" aria-label="Close">
-          <i class="fa-solid fa-clipboard-list"></i> Ordem de Serviços
-        </a>
-      </div>
-    </div> -->
 
-    <div class="row mb-1">
-      <div class="col">
-        <a url="src/empresas/index.php" class="text-decoration-none" data-bs-dismiss="offcanvas" aria-label="Close">
-          <i class="fa-solid fa-clipboard-list"></i> Controle de Ordem de Serviços
-        </a>
-      </div>
-    </div>
-
-
-    <div class="row mb-1">
-      <div class="col">
-        <a url="src/colaboradores/index.php" class="text-decoration-none" data-bs-dismiss="offcanvas" aria-label="Close">
-          <i class="fa-solid fa-clipboard-list"></i> Colaboradores
-        </a>
-      </div>
-    </div>
-
-    <div class="row mb-1">
-      <div class="col">
-        <a url="src/os/status/index.php" class="text-decoration-none" data-bs-dismiss="offcanvas" aria-label="Close">
-          <i class="fa-solid fa-clipboard-list"></i> Status de O.S.
-        </a>
-      </div>
-    </div>
-
-    <div class="row mb-1">
-      <div class="col">
-        <a url="src/os/classificacao/index.php" class="text-decoration-none" data-bs-dismiss="offcanvas" aria-label="Close">
-          <i class="fa-solid fa-clipboard-list"></i> Classificação de O.S.
-        </a>
-      </div>
-    </div>
-
-    <!-- <div class="row mb-1">
-      <div class="col">
-        <i class="fa-solid fa-clipboard-list"></i> Abertura de processos
-      </div>
-    </div> -->
 
   </div>
 </div>
