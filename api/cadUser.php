a4ba9db84edb0361dd833269ea289be3e908cd82
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-28 22:56:21 -0400

    Atualizações

---
+++ api/cadUser.php
@@ -0,0 +1,33 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/app/projectRestaurantes/lib/includes.php");
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
+    $_POST = json_decode(file_get_contents('php://input'), true);
+
+
+    $query = "REPLACE INTO usuarios set
+                                        nome = '{$_POST['nome']}',
+                                        cpf = '{$_POST['cpf']}',
+                                        telefone = '{$_POST['telefone']}',
+                                        titulo = '{$_POST['titulo']}',
+                                        local = '{$_POST['local']}',
+                                        restaurante = '{$_POST['restaurante']}',
+                                        endereco = '{$_POST['endereco']}',
+                                        data = '{$_POST['data']}',
+    ";
+    if(mysqli_query($con, $query)){
+        $retotno = [
+            'status' => true,
+            'message' => 'Tudo certo'
+        ];
+    }else{
+        $retotno = [
+            'status' => false,
+            'message' => 'Deu Erro'
+        ];
+    }
+
+
+    echo json_encode($retotno);
+
+    // file_put_contents('dados.txt', print_r($_POST, true));
---
+++ api/cadVoto.php
@@ -0,0 +1,29 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/app/projectRestaurantes/lib/includes.php");
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
+    $_POST = json_decode(file_get_contents('php://input'), true);
+
+
+    $query = "REPLACE INTO votos set
+                                        restaurante = '{$_POST['restaurante']}',
+                                        usuario = '{$_POST['usuario']}',
+                                        voto = '{$_POST['voto']}',
+                                        data = '{$_POST['data']}',
+    ";
+    if(mysqli_query($con, $query)){
+        $retotno = [
+            'status' => true,
+            'message' => 'Tudo certo'
+        ];
+    }else{
+        $retotno = [
+            'status' => false,
+            'message' => 'Deu Erro'
+        ];
+    }
+
+
+    echo json_encode($retotno);
+
+    // file_put_contents('dados.txt', print_r($_POST, true));
