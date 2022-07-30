21e567ec7db9d2c5b364b30d4a81ac8ab6119351
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-30 07:27:48 -0400

    Atualizações

--- src/home/dashboard/index.php
+++ src/home/dashboard/index.php
@@ -7,7 +7,7 @@
     <h4>Título da página</h4>
 
     <div class="row">
-      <div class="col-md-6">
+      <div class="col">
         <div class="card">
           <h5 class="card-header">Clientes Cadastrados</h5>
           <div class="card-body">
@@ -36,14 +36,54 @@
                 ?>
               </tbody>
             </table>
-
-
-
           </div>
         </div>
       </div>
-
     </div>
+
+
+
+
+
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <h5 class="card-header">Pesquisa de Satisfação</h5>
+          <div class="card-body">
+
+            <table class="table table-striped table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">CPF</th>
+                  <th scope="col">Local</th>
+                  <th scope="col">Voto</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                  $query = "select * from clientes order by codigo desc";
+                  $result = mysqli_query($con, $query);
+                  while($d = mysqli_fetch_object($result)){
+                ?>
+                <tr>
+                  <td><?=$d->cpf?></td>
+                  <td><?=$d->local?></td>
+                  <td>
+                    <i class="fa-regular fa-face-frown"></i>
+                    <i class="fa-regular fa-face-expressionless"></i>
+                    <i class="fa-regular fa-face-smile"></i>
+                  </td>
+                </tr>
+                <?php
+                  }
+                ?>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+
 
   </div>
 </div>
