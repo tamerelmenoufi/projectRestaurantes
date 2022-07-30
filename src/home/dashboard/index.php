1aecb0a7af83907a931a72cf3f7e557f154b2030
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-30 07:29:35 -0400

    Atualizações

--- src/home/dashboard/index.php
+++ src/home/dashboard/index.php
@@ -45,7 +45,7 @@
 
 
 
-    <div class="row">
+    <div class="row" style="margin-top:20px;">
       <div class="col">
         <div class="card">
           <h5 class="card-header">Pesquisa de Satisfação</h5>
