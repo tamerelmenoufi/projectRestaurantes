5cdacfa0c6079fa75abd11d0b909c23af4fd05f7
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-30 07:14:14 -0400

    Atualizações

--- src/home/dashboard/index.php
+++ src/home/dashboard/index.php
@@ -6,5 +6,44 @@
 
     <h4>Título da página</h4>
 
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card">
+          <h5 class="card-header">Clientes Cadastrados</h5>
+          <div class="card-body">
+
+            <table class="table table-striped table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">Nome</th>
+                  <th scope="col">CPF</th>
+                  <th scope="col">Local</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                  $query = "select * from clientes order by codigo desc";
+                  $result = mysqli_query($con, $query);
+                  while($d = mysqli_fetch_object($result)){
+                ?>
+                <tr>
+                  <td><?=$d->nome?></td>
+                  <td><?=$d->cpf?></td>
+                  <td><?=$d->local?></td>
+                </tr>
+                <?php
+                  }
+                ?>
+              </tbody>
+            </table>
+
+
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+
   </div>
 </div>
