eebcbbdf1d0ac2516d645ddde26957acd781d2bd
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-31 23:07:27 -0400

    Atualizações

--- barcode.php
+++ barcode.php
@@ -1,1 +1,7 @@
-<img src="vendor/barcode/?f=png&s=qr&d=tamer mohamed Elmenoufi" width="300" height="300" />
+<?php
+
+$comando = '{"nome":"Tamer Mohamed Elmenoufi","cpf":"601.109.702-25","telefone":"(92) 99188-6570","endereco":"Rua Monsenhor Coutinho, 600, Edifício Maximino Correia, Apto 1302, CEP 69010-110, Manaus,  Amazonas"}';
+
+?>
+
+<img src='lib/vendor/barcode/?f=png&s=qr&d=<?=$comando?>' width="300" height="300" />
