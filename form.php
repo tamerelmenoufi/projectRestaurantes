6eecc9d2f8a3e225586b8ff95c817ad9900d06b0
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-01 10:23:55 -0400

    Atualizações

--- form.php
+++ form.php
@@ -96,9 +96,10 @@
     img{
         position:absolute;
         z-index: 0;
-        bottom:0;
-        right:0;
+        left:50%;
+        margin-left:-50%;
         opacity:0.2;
+        height:100%;
     }
 </style>
 <div class="form">
