942975a7243a0e1cf5f6d8d9c4ff750fe133b202
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-03 11:59:39 -0400

    Atualizações

--- src/home/dashboard/index.php
+++ src/home/dashboard/index.php
@@ -122,11 +122,29 @@
   <!-- Pesquisa de satisfação Gráficos -->
     <div class="row" style="margin-top:20px; margin-bottom:20px;">
 
-      <div class="col-md-12">
+      <div class="col-md-4">
         <div class="card">
           <h5 class="card-header">Votos por Unidade</h5>
           <div class="card-body">
-            <div grafico="votos_unidade"></div>
+            <div grafico="votos_excelente_unidade"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <div class="card">
+          <h5 class="card-header">Votos por Unidade</h5>
+          <div class="card-body">
+            <div grafico="votos_bom_unidade"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <div class="card">
+          <h5 class="card-header">Votos por Unidade</h5>
+          <div class="card-body">
+            <div grafico="votos_ruim_unidade"></div>
           </div>
         </div>
       </div>
