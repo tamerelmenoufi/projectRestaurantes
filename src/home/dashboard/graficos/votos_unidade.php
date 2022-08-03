0352d2fad23af145ce1a2f2a676a25814ab2271e
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-03 11:51:05 -0400

    Atualizações

--- src/home/dashboard/graficos/votos_unidade.php
+++ src/home/dashboard/graficos/votos_unidade.php
@@ -17,7 +17,8 @@ $md5 = md5($_POST['rotulo'].$md5);
             count(*) as qt
         from votos a
         left join restaurantes b on a.restaurante = b.codigo
-        group by a.restaurante, a.voto
+        where a.voto = 'excelente'
+        group by a.restaurante
         order by qt desc
     ";
     $result = mysqli_query($con, $query);
@@ -55,38 +56,7 @@ $md5 = md5($_POST['rotulo'].$md5);
                         ],
                         borderWidth: 1,
                         rotulos: [<?=$R?>]
-                    },
-                    {
-                        label: [<?=$R?>],
-                        data: [<?=$Q?>],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                        ],
-                        borderWidth: 1,
-                        rotulos: [<?=$R?>]
-                    },                    {
-                        label: [<?=$R?>],
-                        data: [<?=$Q?>],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                        ],
-                        borderWidth: 1,
-                        rotulos: [<?=$R?>]
-                    },
+                    }
             ]
             },
             options: {
