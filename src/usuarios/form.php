c9a8669101938c04b1c2e8e75cfb89c3a3492dbb
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-02 23:48:47 -0400

    Atualizações

--- src/usuarios/form.php
+++ src/usuarios/form.php
@@ -1,1 +1,130 @@
-Formulario
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/app/projectRestaurantes/lib/includes.php");
+
+
+    if($_POST['acao'] == 'salvar'){
+
+        $data = $_POST;
+        $attr = [];
+
+        unset($data['codigo']);
+        unset($data['acao']);
+
+        foreach ($data as $name => $value) {
+            $attr[] = "{$name} = '" . mysqli_real_escape_string($con, $value) . "'";
+        }
+        $attr = implode(', ', $attr);
+
+        if($_POST['codigo']){
+            $query = "update clientes set {$attr} where codigo = '{$_POST['codigo']}'";
+            mysqli_query($con, $query);
+            $cod = $_POST['codigo'];
+        }else{
+            $query = "insert into clientes set data_cadastro = NOW(), {$attr}";
+            mysqli_query($con, $query);
+            $cod = mysqli_insert_id($con);
+        }
+
+        $retorno = [
+            'status' => true,
+            'codigo' => $cod
+        ];
+
+        echo json_encode($retorno);
+
+        exit();
+    }
+
+
+    $query = "select * from clientes where codigo = '{$_POST['cod']}'";
+    $result = mysqli_query($con, $query);
+    $d = mysqli_fetch_object($result);
+?>
+<style>
+    .Titulo<?=$md5?>{
+        position:absolute;
+        left:60px;
+        top:8px;
+        z-index:0;
+    }
+</style>
+<h4 class="Titulo<?=$md5?>">Cadastro do Usuário</h4>
+    <form id="form-<?= $md5 ?>">
+        <div class="row">
+            <div class="col">
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome completo" value="<?=$d->nome?>">
+                    <label for="nome">Nome*</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="text" name="cpf" id="cpf" class="form-control" placeholder="CPF" value="<?=$d->cpf?>">
+                    <label for="cpf">CPF*</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="text" name="telefone" id="telefone" class="form-control" placeholder="telefone" value="<?=$d->telefone?>">
+                    <label for="telefone">Telefone*</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="text" name="endereco" id="endereco" class="form-control" placeholder="Endereço" value="<?=$d->endereco?>">
+                    <label for="endereco">Endereço Completo</label>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <div style="display:flex; justify-content:end">
+                    <button type="submit" SalvarFoto class="btn btn-success btn-ms">Salvar</button>
+                    <input type="hidden" id="codigo" value="<?=$_POST['cod']?>" />
+                </div>
+            </div>
+        </div>
+    </form>
+
+    <script>
+        $(function(){
+            Carregando('none');
+            $('#form-<?=$md5?>').submit(function (e) {
+
+                e.preventDefault();
+
+                var codigo = $('#codigo').val();
+                var campos = $(this).serializeArray();
+
+                if (codigo) {
+                    campos.push({name: 'codigo', value: codigo})
+                }
+
+                campos.push({name: 'acao', value: 'salvar'})
+
+                Carregando();
+
+                $.ajax({
+                    url:"src/usuarios/form.php",
+                    type:"POST",
+                    typeData:"JSON",
+                    mimeType: 'multipart/form-data',
+                    data: campos,
+                    success:function(dados){
+                        // if(dados.status){
+                            $.ajax({
+                                url:"src/usuarios/index.php",
+                                type:"POST",
+                                success:function(dados){
+                                    $("#paginaHome").html(dados);
+                                }
+                            });
+                        // }
+                    },
+                    error:function(erro){
+
+                        // $.alert('Ocorreu um erro!' + erro.toString());
+                        //dados de teste
+                    }
+                });
+
+            });
+
+        })
+    </script>
