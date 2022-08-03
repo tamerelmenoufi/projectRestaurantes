4ff33d2478ae0f4dda8fa9d9c5e3b2ce734b29b0
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-03 11:00:36 -0400

    Atualizações

--- src/home/dashboard/graficos/tipo_cadastros.php
+++ src/home/dashboard/graficos/tipo_cadastros.php
@@ -10,7 +10,7 @@ $md5 = md5($_POST['rotulo'].$md5);
 
 <?php
 
-    echo $query = "
+    $query = "
     SELECT
         (SELECT count(*) as app FROM `clientes` where local != '') as app,
         (SELECT count(*) as app FROM `clientes` where local = '') as cartao
