584172aa6523dadb755f56a3a8e8207f36566a96
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-03 15:09:13 -0400

    Atualizações

--- src/home/dashboard/graficos/votos_bom_unidade.php
+++ src/home/dashboard/graficos/votos_bom_unidade.php
@@ -44,16 +44,18 @@ $md5 = md5($_POST['rotulo'].$md5);
                     {
                         label: [<?=$R?>],
                         data: [<?=$Q?>],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                        ],
+                        backgroundColor: Utils.CHART_COLORS.orange,
+                        // [
+                        //     'rgba(255, 99, 132, 0.2)',
+                        //     'rgba(54, 162, 235, 0.2)',
+                        //     'rgba(255, 206, 86, 0.2)',
+                        // ],
+                        borderColor: Utils.CHART_COLORS.orange,
+                        // [
+                        //     'rgba(255, 99, 132, 1)',
+                        //     'rgba(54, 162, 235, 1)',
+                        //     'rgba(255, 206, 86, 1)',
+                        // ],
                         borderWidth: 1,
                         rotulos: [<?=$R?>]
                     }
