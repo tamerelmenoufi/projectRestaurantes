f76ecb7905c6eadc630cfbcf0608755d05cf650d
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-05 13:18:45 -0400

    Atualizações

--- src/usuarios/index.php
+++ src/usuarios/index.php
@@ -53,7 +53,6 @@
 
                   <div class="form-check form-switch">
                     <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
-                    <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
                   </div>
 
                   </td>
