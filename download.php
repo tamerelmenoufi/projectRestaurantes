bb2abb39d70fe5cdd1057692e7f9fcf334a23a30
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-08 15:31:04 -0400

    Atualizações

--- download.php
+++ download.php
@@ -1,5 +1,4 @@
 <?php
-
     $dir = opendir("src/home/dashboard/csv/");
     while($v = readdir($dir)){
         if($v != '.' && $v != '..'){
@@ -8,5 +7,11 @@
     }
     $file = $_GET['f'].".php";
     if(in_array($file,$Files)) {
+        session_start();
+        header('Content-Type: application/csv');
+        header('Content-Disposition: attachment; '.$_GET['f'].'-'.$_SESSION['busca_data'].'.csv');
+        header('Pragma: no-cache');
+
+
         include("src/home/dashboard/csv/{$file}");
     }
